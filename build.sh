75d4029464df562b1770c7f0468a448748839891
Auston Stewart <auston@shedbuilt.net>
2018-01-09 18:10:33 -0500

    Updating to coreutils 8.29

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-patch -Np1 -i ${SHED_PATCHDIR}/coreutils-8.27-i18n-1.patch
+patch -Np1 -i ${SHED_PATCHDIR}/coreutils-8.29-i18n-1.patch
 FORCE_UNSAFE_CONFIGURE=1 ./configure \
             --prefix=/usr            \
-            --enable-no-install-program=kill,uptime
-FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUMJOBS
-make DESTDIR=${SHED_FAKEROOT} install
+            --enable-no-install-program=kill,uptime || exit 1
+FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUMJOBS || exit 1
+make DESTDIR=${SHED_FAKEROOT} install || exit 1
 mkdir -v ${SHED_FAKEROOT}/bin
 mv -v ${SHED_FAKEROOT}/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} ${SHED_FAKEROOT}/bin
 mv -v ${SHED_FAKEROOT}/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} ${SHED_FAKEROOT}/bin
@@ -14,4 +14,4 @@ mv -v ${SHED_FAKEROOT}/usr/bin/chroot ${SHED_FAKEROOT}/usr/sbin
 mkdir -pv ${SHED_FAKEROOT}/usr/share/man/man8
 mv -v ${SHED_FAKEROOT}/usr/share/man/man1/chroot.1 ${SHED_FAKEROOT}/usr/share/man/man8/chroot.8
 sed -i s/\"1\"/\"8\"/1 ${SHED_FAKEROOT}/usr/share/man/man8/chroot.8
-mv -v ${SHED_FAKEROOT}/usr/bin/{head,sleep,nice,test,[} ${SHED_FAKEROOT}/bin
+mv -v ${SHED_FAKEROOT}/usr/bin/{head,sleep,nice} ${SHED_FAKEROOT}/bin
