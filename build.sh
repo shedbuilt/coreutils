791caea3a5f8a2ab2a9ce3863b8a6a85ed428432
Auston Stewart <auston@shedbuilt.net>
2018-04-15 09:36:58 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-case "$SHED_BUILDMODE" in
+case "$SHED_BUILD_MODE" in
     toolchain)
         FORCE_UNSAFE_CONFIGURE=1 \
         ./configure --prefix=/tools \
                     --enable-install-program=hostname && \
-        make -j $SHED_NUMJOBS && \
-        make DESTDIR="$SHED_FAKEROOT" install
+        make -j $SHED_NUM_JOBS && \
+        make DESTDIR="$SHED_FAKE_ROOT" install
         ;;
     *)
-        patch -Np1 -i "${SHED_PATCHDIR}/coreutils-8.29-i18n-1.patch" && \
+        patch -Np1 -i "${SHED_PKG_PATCH_DIR}/coreutils-8.29-i18n-1.patch" && \
         autoreconf -fiv && \
         FORCE_UNSAFE_CONFIGURE=1 ./configure \
                     --prefix=/usr            \
                     --enable-no-install-program=kill,uptime && \
-        FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUMJOBS && \
-        make DESTDIR="$SHED_FAKEROOT" install || exit 1
-        mkdir -v "${SHED_FAKEROOT}/bin"
-        mv -v "${SHED_FAKEROOT}"/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} "${SHED_FAKEROOT}/bin"
-        mv -v "${SHED_FAKEROOT}"/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} "${SHED_FAKEROOT}/bin"
-        mv -v "${SHED_FAKEROOT}"/usr/bin/{rmdir,stty,sync,true,uname} "${SHED_FAKEROOT}/bin"
-        mkdir -pv "${SHED_FAKEROOT}/usr/sbin"
-        mv -v "${SHED_FAKEROOT}/usr/bin/chroot" "${SHED_FAKEROOT}/usr/sbin"
-        mkdir -pv "${SHED_FAKEROOT}/usr/share/man/man8"
-        mv -v "${SHED_FAKEROOT}/usr/share/man/man1/chroot.1" "${SHED_FAKEROOT}/usr/share/man/man8/chroot.8"
-        sed -i s/\"1\"/\"8\"/1 "${SHED_FAKEROOT}/usr/share/man/man8/chroot.8"
-        mv -v "${SHED_FAKEROOT}"/usr/bin/{head,sleep,nice} "${SHED_FAKEROOT}/bin"
+        FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUM_JOBS && \
+        make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+        mkdir -v "${SHED_FAKE_ROOT}/bin"
+        mv -v "${SHED_FAKE_ROOT}"/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} "${SHED_FAKE_ROOT}/bin"
+        mv -v "${SHED_FAKE_ROOT}"/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} "${SHED_FAKE_ROOT}/bin"
+        mv -v "${SHED_FAKE_ROOT}"/usr/bin/{rmdir,stty,sync,true,uname} "${SHED_FAKE_ROOT}/bin"
+        mkdir -pv "${SHED_FAKE_ROOT}/usr/sbin"
+        mv -v "${SHED_FAKE_ROOT}/usr/bin/chroot" "${SHED_FAKE_ROOT}/usr/sbin"
+        mkdir -pv "${SHED_FAKE_ROOT}/usr/share/man/man8"
+        mv -v "${SHED_FAKE_ROOT}/usr/share/man/man1/chroot.1" "${SHED_FAKE_ROOT}/usr/share/man/man8/chroot.8"
+        sed -i s/\"1\"/\"8\"/1 "${SHED_FAKE_ROOT}/usr/share/man/man8/chroot.8"
+        mv -v "${SHED_FAKE_ROOT}"/usr/bin/{head,sleep,nice} "${SHED_FAKE_ROOT}/bin"
         ;;
 esac
