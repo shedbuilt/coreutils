93acc64b06ee1a6cc184cf9b47ecc54855449ecd
Auston Stewart <auston@shedbuilt.net>
2018-05-30 17:03:38 -1000

    Updating for blank

--- build.sh
+++ build.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 SHED_PKG_LOCAL_PREFIX='/usr'
+SHED_PKG_LOCAL_PROGRAM_INSTALL_OPTIONS='--enable-no-install-program=kill,uptime'
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
     SHED_PKG_LOCAL_PREFIX='/tools'
+    SHED_PKG_LOCAL_PROGRAM_INSTALL_OPTIONS='--enable-install-program=hostname'
 fi
-
-# Configure and Build
-if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
-    FORCE_UNSAFE_CONFIGURE=1 \
-    ./configure --prefix=${SHED_PKG_LOCAL_PREFIX} \
-                --enable-install-program=hostname &&
-    FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUM_JOBS &&
-    make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
-else
-    patch -Np1 -i "${SHED_PKG_PATCH_DIR}/coreutils-8.29-i18n-1.patch" &&
-    autoreconf -fiv &&
-    FORCE_UNSAFE_CONFIGURE=1 \
-    ./configure --prefix=${SHED_PKG_LOCAL_PREFIX} \
-                --enable-no-install-program=kill,uptime &&
-    FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUM_JOBS &&
-    make DESTDIR="$SHED_FAKE_ROOT" install &&
+# Patch
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/coreutils-8.29-i18n-1.patch" &&
+autoreconf -fiv &&
+# Configure
+FORCE_UNSAFE_CONFIGURE=1 \
+./configure --prefix=$SHED_PKG_LOCAL_PREFIX \
+            $SHED_PKG_LOCAL_PROGRAM_INSTALL_OPTIONS &&
+# Build and Install
+FORCE_UNSAFE_CONFIGURE=1 make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Rearrange
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
     mkdir -v "${SHED_FAKE_ROOT}/bin" &&
     mv -v "${SHED_FAKE_ROOT}"/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} "${SHED_FAKE_ROOT}/bin" &&
     mv -v "${SHED_FAKE_ROOT}"/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} "${SHED_FAKE_ROOT}/bin" &&
